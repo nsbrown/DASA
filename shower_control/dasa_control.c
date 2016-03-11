317454bf045720c4d38baa16cf68af5d7788d1cc
Earl Bombard <ewb09190@gmail.com>
2016-03-11 10:48:40 -0500

    added data driven functionality to shower control program

--- shower_control/dasa_control.c
+++ shower_control/dasa_control.c
@@ -1,131 +1,97 @@
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
 #include <string.h>
-//#include <delay.h>
 
 #define BUFFER_SIZE 256
-//FILE *input;
 
-int main(int argc, char **argv)
+struct commandData 
+{
+		char* word;
+		void (*fp)(struct Command*, int);
+		struct commandData* nextArray;	
+		int arg;
+};
 
+
+//--------------High Level Array-------------------
+struct commandData highLevel[]
+{
+	{"ON\n", power, 0 , 1}, {"OFF\n", power, 0, 0}
+	//{"Move", processNextWord(), }, {"Up", processNextWord(),   },
+	//{"Down", processNextWord(), }, {"Right", processNextWord(),  },
+	//{"Left"}, {"Motion"},
+	//{"Manuever"}, {"Forward"},
+	//{"Back"}, {"Backwards"},
+	//{"Temp"}, {"Temperature"},
+	//{"Hotter"}, {"Warmer"},
+	//{"Colder"}, {"Cooler"},
+	//{"Cool"}, {"Heat"},
+	
+};
+//--------------High Level Secondary Array--------
+//struct commandData highLevelSec[] =
+//{
+//	{"A"}, {""},
+//	{}, {},
+//	{}, {},
+//	{}, {},
+//};
+
+//--------------Name Array-------------------------
+struct commandData dasaNames[] = 
+{	//{"Daisy",proccessNextWord(),highLevel}, 
+	//{"Donald" proccessNextWord(), highLevel},
+	{"DASA", proccessNextWord, highLevel, 0}, 
+	{0,0,0,0}
+};
+
+
+
+//--------------
+
+void power(struct commandData*, int arg)
+{
+	
+	if(arg == 1)
+	{
+		printf("TURNING ON\n");
+		system("./dasademo ON");
+	}
+	else if(arg == 0)
+	{
+		printf("TURNING OFF\n");
+		system("./dasademo OFF");
+	}
+}
+
+void processNextWord(sruct commandData* words, int arg)
 {
 	FILE *input;
 	input = stdin;
-	char *buffer;
-	buffer = (char *)malloc(BUFFER_SIZE);
+	char buffer[BUFFER_SIZE];
+	
+	for(int i = 0; words[i].word != 0 ;i++)
+	{
+		if(strcasecmp(buffer, words[i].word) == 0)
+		{
+			(*words[i].fp)(words.nextArray, arg);
+			return;
+		}
+	}
+	printf("word %s not found\n", buffer);
+}
+
+int main()
+{
+	
 	//sleep(500);
 	printf("RUNNING COMMAND SCRIPT");
 
 	for(;;)
 	{
-		char * result = fgets(buffer, BUFFER_SIZE, input);
-		if (result == NULL) {
-			if(!feof(input)) {
-				perror("fgets returned null");
-			}
-			else {break;}
-			continue;
-		}
-		if(strcmp(buffer, "DASA ON\n") == 0)
-		{
-			printf("TURNING ON\n");
-			system("./dasademo ON");
-		}
-		else if(strcmp(buffer, "DASA OFF\n") == 0)
-		{
-			printf("TURNING OFF\n");
-			system("./dasademo OFF");
-		}
-		else if(strcmp(buffer, "DASA STOP\n") == 0)
-		{
-			printf("STOPPING CURRENT ACTION\n");
-
-		}
-		else if(strcmp(buffer, "DASA RESET\n") == 0)
-		{
-			printf("RESETTING\n");
-
-		}
-		else if(strcmp(buffer, "DASA BACK\n") == 0)
-		{
-			printf("MOVING AWAY\n");
-
-		}
-		else if(strcmp(buffer, "DASA FORWARD\n") == 0)
-		{
-			printf("MOVING FORWARD\n");
-
-		}
-		else if(strcmp(buffer, "DASA LEFT\n") == 0)
-		{
-			printf("MOVING LEFT\n");
-			system("./dasademo x 10");
-
-		}
-		else if(strcmp(buffer, "DASA RIGHT\n") == 0)
-		{
-			printf("MOVING RIGHT\n");
-			system("./dasademo x 10");
-
-		}
-		else if(strcmp(buffer, "DASA DOWN\n") == 0)
-		{
-			printf("MOVING DOWN\n");
-			system("./dasademo y 10");
-
-		}
-		else if(strcmp(buffer, "DASA UP\n") == 0)
-		{
-			printf("MOVING UP\n");
-			system("./dasademo y 10");
-
-		}
-		else if(strcmp(buffer, "DASA MODE SHOWER\n") == 0)
-		{
-			printf("CHANGING TO SHOWR MODE\n");
-			system("./dasademo SHOWER");
-
-		}
-		else if(strcmp(buffer, "DASA MODE JET\n") == 0)
-		{
-			printf("CHANGING TO JET MODE\n");
-			system("./dasademo JET");
-
-		}
-		else if(strcmp(buffer, "DASA MODE MASSAGE\n") == 0)
-		{
-			printf("CHANGING TO MASSAGE MODE\n");
-			system("./dasademo MASSAGE");
-
-		}
-		else if(strcmp(buffer, "DASA MODE RAIN\n") == 0)
-		{
-			printf("CHANGING TO RAIN MODE\n");
-			system("./dasademo RAIN");
-
-		}
-		else if(strcmp(buffer, "DASA TEMPERATURE LOWER\n") == 0)
-		{
-			printf("LOWERING TEMPERATURE\n");
-
-		}
-		else if(strcmp(buffer, "DASA TEMPERATURE HIGHER\n") == 0)
-		{
-			printf("INCREASING TEMPERATURE\n");
-
-		}
-		else
-		{
-			printf("failure to recognize, %s try again\n", buffer);
-			
-		}
-
+		proccessNextWord(dasaNames, 0);
+		
 	}
-	return EXIT_SUCCESS;
 }
-
 
 
 
